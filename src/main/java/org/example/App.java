0a0781cab80d2e9a179aaac7d0ddca157da8f6dc
tureiko <antonina03032007>
2022-09-11 00:28:10 +0300

    д.з.2

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,18 +4,16 @@ package org.example;
 public class App{
 
 
-    public static void main(String[]args)
+    public static void main(String[]args){
 
 
 
-    {
         System.out.println(fibonazzi(0));}
-
 
 
     public static long fibonazzi(int n){
 
-    return n<=0?0: n<=2 ? 1 : fibonazzi(n-2)+fibonazzi(n-1);}
+    return n<=1?n: fibonazzi(n-2)+fibonazzi(n-1);}
 
     }
 
