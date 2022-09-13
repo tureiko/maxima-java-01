7a4e092ba177d7a3f333bb5e9adb86eb1980a998
tureiko <antonina03032007>
2022-09-13 18:39:01 +0300

    д.з.4

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,41 +6,33 @@ public class App {
 
     public static void main(String[] args) {
 
-         System.out.println( daysCount(2,2020));}
-
-    public static byte daysCount(int month,int year){
-        byte result;
-        switch (month){
-
-            case 1:
-            case 3:
-            case 5:
-            case 7:
-            case 8:
-            case 10:
-            case 12:
-                result =31;
-                break;
-            case 4:
-            case 6:
-            case 9:
-            case 11:
-                result=30;
-                break;
-            case 2:
-                if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
-                    result=29;}
-                else{
-                    result=28;}
-
-
-                break;
-            default: throw new IllegalStateException("Unexpected value: " + month);
-        }       return result;
+        System.out.println(isSimple(5));
     }
 
 
+    public static int isSimple(int n) {
+
+
+        for (int i = 2; i < n; i++) {
+
+            if (n / i * i == n) {
+                n = 1;
+            } else {
+                n = 0;
+                break;
+            }
+
         }
+
+        return n;
+    }
+}
+
+
+
+
+
+
 
 
 
