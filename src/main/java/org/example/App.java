3e5262ca11799172ae960b31bf6381fffdad68ea
tureiko <antonina03032007>
2022-09-16 22:55:35 +0300

    д.з.4.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -6,22 +6,14 @@ public class App {
 
     public static void main(String[] args) {
 
-        System.out.println(isSimple(15));
+        System.out.println(isSimple(10));
     }
-
-
     public static int isSimple(int n) {
 
-
         for (int i = 2; i < n; i++) {
-
-
             if (n % i == 0) {
-
-                return i;
-            }
-        }
-        return n = 0;
+                return i;}
+        }return  0;
     }
 }
 
