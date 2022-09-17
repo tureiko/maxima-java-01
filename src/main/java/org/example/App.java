1a081989118ea873c72dca8c92b3f2a61ffa3433
tureiko <antonina03032007>
2022-09-17 16:01:23 +0300

    д.з.4..

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -10,23 +10,14 @@ public class App {
     }
     public static int isSimple(int n) {
 
-        for (int i = 2; i < n; i++) {
+        for (int i = 2;i<n/2; i++) {
             if (n % i == 0) {
-                return i;}
-        }return  0;
+                return i;
+            }
+        }return 0;
+
     }
 }
-
-
-
-
-
-
-
-
-
-
-
 
 
 
