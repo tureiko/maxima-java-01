6ebe1b0feae4e38e1a5a8b70083fbc0dfcd32e9d
tureiko <antonina03032007>
2022-10-18 19:20:42 +0300

    д.з.5

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,23 +1,74 @@
 package org.example;
-
-
 public class App {
 
-
     public static void main(String[] args) {
-
-        System.out.println(isSimple(10));
+        int [] arr = {43,81,92,14,22,51,18,29};
+        System.out.println(+maxDigitsSumPosition(arr));
     }
-    public static int isSimple(int n) {
 
-        for (int i = 2;i<n/2; i++) {
-            if (n % i == 0) {
-                return i;
+    public static byte maxDigitsSumPosition(int[] arr) {
+        byte indexmax = 0;
+        byte indexmax1 = 0;
+        int num = arr[0];
+        int sumDig = 0;
+        int[] arrNums = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            int counter = arr[i];
+            int sum = 0;
+            int n = counter;
+
+            while (n != 0) {
+                sum = sum + (n % 10);       //Суммирование цифр числа
+                n = n / 10;
+                sumDig = sum;
+                arrNums[i] = sumDig;
             }
-        }return 0;
+
+            if (num < counter) {
+                num = counter;
+                indexmax = (byte) i;
+            }
+        }
+        int num1 = arrNums[0];
+        for (int j = 0; j < arrNums.length; j++) {
+            int counter1 = arrNums[j];
+
+           if (num1 <= counter1) {
+                num1 = counter1;
+                indexmax1 = (byte) j;
+            }
+        }
+        return indexmax1;
 
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
