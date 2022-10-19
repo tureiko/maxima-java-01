d25302d402d0b3a54bea8ff86cb585a780b64904
tureiko <antonina03032007>
2022-10-19 22:55:56 +0300

    д.з.5.

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -1,59 +1,34 @@
+
 package org.example;
 public class App {
 
     public static void main(String[] args) {
-        int [] arr = {43,81,92,14,22,51,18,29};
-        System.out.println(+maxDigitsSumPosition(arr));
+        int [] arr = {43,92,93,39,11};
+        System.out.println(maxDigitsSumPosition(arr));
     }
-
     public static byte maxDigitsSumPosition(int[] arr) {
         byte indexmax = 0;
-        byte indexmax1 = 0;
-        int num = arr[0];
-        int sumDig = 0;
         int[] arrNums = new int[arr.length];
-
+        int num=arrNums[0];
         for (int i = 0; i < arr.length; i++) {
             int counter = arr[i];
-            int sum = 0;
+            int sum=0;
             int n = counter;
-
+            int sumDig=0;
             while (n != 0) {
                 sum = sum + (n % 10);       //Суммирование цифр числа
                 n = n / 10;
                 sumDig = sum;
-                arrNums[i] = sumDig;
-            }
+               arrNums[i] = sumDig;
 
-            if (num < counter) {
-                num = counter;
-                indexmax = (byte) i;
-            }
-        }
-        int num1 = arrNums[0];
-        for (int j = 0; j < arrNums.length; j++) {
-            int counter1 = arrNums[j];
-
-           if (num1 <= counter1) {
-                num1 = counter1;
-                indexmax1 = (byte) j;
+                if (num<=sumDig) {
+                   num= sumDig;
+                    indexmax = (byte) i;
+                }
             }
         }
-        return indexmax1;
-
-    }
-}
-
-
-
-
-
-
-
-
-
-
-
+        return indexmax;
+    }}
 
 
 
